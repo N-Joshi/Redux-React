e7e24f2bdc339f8a0db32a3f4d6522d0686783e8
Joshi <679994@cognizant.com>
2018-12-11 17:53:50 +0530

    'Final'

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {Provider} from "react-redux";
 import {createStore} from "redux";
 import allReducers from "./reducers/combineReducer";
  
-const reduxStore =  createStore(allReducers)
+const reduxStore =  createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 ReactDOM.render(
